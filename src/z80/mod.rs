c1aeb1962ff8906cc63e3bd75aa225204890d907
Ian Knight <ian.knight2@nottingham.ac.uk>
2023-03-14 15:53:23 +0000

    Move registers into separate struct

--- src/z80/mod.rs
+++ src/z80/mod.rs
@@ -1,1 +1,24 @@
-pub mod register;
+use self::register::RegisterSet;
+
+mod opcode;
+mod register;
+
+const ROM_SIZE: usize = 16 * 1024;
+
+const RAM_SIZE: usize = 48 * 1024;
+
+pub struct Z80Cpu {
+    rom: [u8; ROM_SIZE],
+    ram: [u8; RAM_SIZE],
+    registers: RegisterSet,
+}
+
+impl Z80Cpu {
+    pub fn new() -> Z80Cpu {
+        Z80Cpu { 
+            rom: [0; ROM_SIZE],
+            ram: [0; RAM_SIZE],
+            registers: RegisterSet::new(),
+        }
+    }
+}
--- src/z80/register.rs
+++ src/z80/register.rs
@@ -1,45 +1,143 @@
-use std::path::MAIN_SEPARATOR;
-
-pub struct Registers {
-    main_regs: [u16; 4],
-    alt_regs: [u16; 4],
-    index_x: u16,
-    index_y: u16,
-    stack_pointer: u16,
-    interrupt: u8,
-    refresh: u8,
-    prog_counter: u16,
+#[derive(Debug)]
+pub enum Register {
+    A,
+    A1,
+    F,
+    F1,
+    BC,
+    BC1,
+    B,
+    B1,
+    C,
+    C1,
+    DE,
+    DE1,
+    D,
+    D1,
+    E,
+    E1,
+    HL,
+    HL1,
+    H,
+    H1,
+    L,
+    L1,
+    IX,
+    IY,
+    SP,
+    I,
+    R,
+    PC,
 }
 
-impl Registers {
-    pub fn new() -> Registers {
-        Registers { 
-            main_regs: [0; 4],
-            alt_regs: [0; 4],
-            index_x: 0,
-            index_y: 0,
-            stack_pointer: 0,
-            interrupt: 0,
-            refresh: 0,
-            prog_counter: 0,
+#[inline]
+fn high_reg(reg: u16) -> u16 {
+    (reg & 0xff00) >> 8
+}
+
+#[inline]
+fn set_high_reg(reg: u16, value: u16) -> u16 {
+    (reg & 0x00ff) | ((value & 0x00ff) << 8)
+}
+
+#[inline]
+fn low_reg(reg: u16) -> u16 {
+    reg & 0x00ff
+}
+
+#[inline]
+fn set_low_reg(reg: u16, value: u16) -> u16 {
+    (reg & 0xff00) | (value & 0x00ff)
+}
+
+#[derive(Default)]
+pub struct RegisterSet {
+    af: u16,
+    af1: u16,
+    bc: u16,
+    bc1: u16,
+    de: u16,
+    de1: u16,
+    hl: u16,
+    hl1: u16,
+    ix: u16,
+    iy: u16,
+    sp: u16,
+    i: u8,
+    r: u8,
+    pc: u16,
+}
+
+impl RegisterSet {
+    pub fn new() -> RegisterSet {
+        RegisterSet {
+            ..Default::default()
         }
     }
 
-    pub fn get(&self, name: &str) -> Option<u16> {
-        match name {
-            "AF" => Some(self.main_regs[0]),
-            "A" => Some((self.main_regs[0] & 0xff00) >> 8),
-            "BC" => Some(self.main_regs[1]),
-            "B" => Some((self.main_regs[1] & 0xff00) >> 8),
-            "C" => Some(self.main_regs[1] & 0x00ff),
-            "DE" => Some(self.main_regs[2]),
-            "D" => Some((self.main_regs[2] & 0xff00) >> 8),
-            "E" => Some(self.main_regs[2] & 0x00ff),
-            "HL" => Some(self.main_regs[3]),
-            "H" => Some((self.main_regs[3] & 0xff00) >> 8),
-            "L" => Some(self.main_regs[3] & 0x00ff),
-            _ => None,
+    pub fn get_reg(&self, reg: Register) -> u16 {
+        match reg {
+            Register::A => high_reg(self.af),
+            Register::A1 => high_reg(self.af1),
+            Register::F => low_reg(self.af),
+            Register::F1 => low_reg(self.af1),
+            Register::BC => self.bc,
+            Register::BC1 => self.bc1,
+            Register::B => high_reg(self.bc),
+            Register::B1 => high_reg(self.bc1),
+            Register::C => low_reg(self.bc),
+            Register::C1 => low_reg(self.bc1),
+            Register::DE => self.de,
+            Register::DE1 => self.de1,
+            Register::D => high_reg(self.de),
+            Register::D1 => high_reg(self.de1),
+            Register::E => low_reg(self.de),
+            Register::E1 => low_reg(self.de1),
+            Register::HL => self.hl,
+            Register::HL1 => self.hl1,
+            Register::H => high_reg(self.hl),
+            Register::H1 => high_reg(self.hl1),
+            Register::L => low_reg(self.hl),
+            Register::L1 => low_reg(self.hl1),
+            Register::IX => self.ix,
+            Register::IY => self.iy,
+            Register::SP => self.sp,
+            Register::I => self.i as u16,
+            Register::R => self.r as u16,
+            Register::PC => self.pc,
+        }
+    }
+
+    pub fn set_reg(&mut self, reg: Register, value: u16) {
+        match reg {
+            Register::A => self.af = set_high_reg(self.af, value),
+            Register::A1 => self.af1 = set_high_reg(self.af1, value),
+            Register::F => self.af = set_low_reg(self.af, value),
+            Register::F1 => self.af1 = set_low_reg(self.af1, value),
+            Register::BC => self.bc = value,
+            Register::BC1 => self.bc1 = value,
+            Register::B => self.af = set_high_reg(self.bc, value),
+            Register::B1 => self.af1 = set_high_reg(self.bc1, value),
+            Register::C => self.af = set_low_reg(self.bc, value),
+            Register::C1 => self.af1 = set_low_reg(self.bc1, value),
+            Register::DE => self.de = value,
+            Register::DE1 => self.de1 = value,
+            Register::D => self.af = set_high_reg(self.de, value),
+            Register::D1 => self.af1 = set_high_reg(self.de1, value),
+            Register::E => self.af = set_low_reg(self.de, value),
+            Register::E1 => self.af1 = set_low_reg(self.de1, value),
+            Register::HL => self.hl = value,
+            Register::HL1 => self.de1 = value,
+            Register::H => self.af = set_high_reg(self.hl, value),
+            Register::H1 => self.af1 = set_high_reg(self.hl1, value),
+            Register::L => self.af = set_low_reg(self.hl, value),
+            Register::L1 => self.af1 = set_low_reg(self.hl1, value),
+            Register::IX => self.ix = value,
+            Register::IY => self.iy = value,
+            Register::SP => self.sp = value,
+            Register::I => self.i = (value & 0x00ff) as u8,
+            Register::R => self.r = (value & 0x00ff) as u8,
+            Register::PC => self.pc = value,
         }
     }
 }
-
